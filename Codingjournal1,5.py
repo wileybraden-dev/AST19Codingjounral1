2526c0cca17b510a9f03ae6366936a543a4117a7
wileybraden-dev <wileybraden@gmail.com>
2026-04-17 22:02:40 -0700

    Add files via upload

---
+++ Codingjournal1,5.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 13:14:46 2026
+
+@author: wiley
+"""
+
+
+from astropy.table import Table
+import numpy as np
+
+x = np.linspace(0, 2*np.pi, 1000)
+print(x)
+
+y = np.sin(x)
+
+data = Table([x,y],names=['x','y'])
+def main():
+    print(data)
+    
+if __name__ =='__main__':
+    main()
+
+
